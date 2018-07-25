74498634a7dbcc92f53c6f3316a0fa385a60f238
TheBrokenRail <connor24nolan@live.com>
2018-07-25 15:37:34 -0400

    Update

--- Code.py
+++ Code.py
@@ -9,27 +9,33 @@ output_data = tf.placeholder(dtype=tf.float32, shape=None)
 slope = tf.Variable(0.5, dtype=tf.float32)
 intercept = tf.Variable(0.1, dtype=tf.float32)
 
-model_operation = slope * input_data % intercept
+model_operation = input_data * slope + intercept
 
 error = model_operation - output_data
 squared_error = tf.square(error)
 loss = tf.reduce_mean(squared_error)
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00001)
 train = optimizer.minimize(loss)
 
 init = tf.global_variables_initializer()
 
-x_values = [0, 1, 2, 3, 4]
-y_values = [1, 3, 5, 7, 9]
+start_values = [0, 1, 2, 3, 4]
+end_values = [1, 3, 5, 7, 9]
 
 with tf.Session() as sess:
     sess.run(init)
-    for i in range(2000000):
-        sess.run(train, feed_dict={input_data: x_values, output_data: y_values})
+    trainCount = 200000
+    previousProgress = -1
+    for i in range(trainCount):
+        sess.run(train, feed_dict={input_data: start_values, output_data: end_values})
         if i % 100 == 0:
-            print(sess.run([slope, intercept]))
-    print("Loss: " + str(sess.run(loss, feed_dict={input_data: x_values, output_data: y_values})))
-    plt.plot(x_values, y_values, 'ro', 'Training Data')
-    plt.plot(x_values, sess.run(model_operation, feed_dict={input_data: x_values}))
+            if int(i / trainCount * 100) > previousProgress:
+                previousProgress = int(i / trainCount * 100)
+                print("Progress: " + str(previousProgress) + "%")
+            plt.plot(start_values, sess.run(model_operation, feed_dict={input_data: start_values}))
+    print(sess.run([slope, intercept]))
+    print("Loss: " + str(sess.run(loss, feed_dict={input_data: start_values, output_data: end_values})))
+    plt.plot(start_values, end_values, 'ro', 'Training Data')
+    plt.plot(start_values, sess.run(model_operation, feed_dict={input_data: start_values}))
     plt.show()
